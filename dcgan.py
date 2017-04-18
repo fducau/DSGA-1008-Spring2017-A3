dbd47e5a8d617654ea30973b2f89ff4a0d7e2bfb
fducau <fnd212@nyu.edu>
2017-04-18 19:56:21 -0400

    First approach

--- dcgan.py
+++ dcgan.py
@@ -16,11 +16,12 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
-
+from itertools import cycle
+from itertools import izip
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataroot_real', required=True, help='path to dataset')
-parser.add_argument('--dataroot_fake', required=True, help='path to dataset')
+parser.add_argument('--dataroot_real', help='path to dataset', default='./data/imagenet/')
+parser.add_argument('--dataroot_fake', help='path to dataset', default='./data/fake/')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
 parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
@@ -34,8 +35,9 @@ parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
-parser.add_argument('--outf', default='.', help='folder to output images and model checkpoints')
+parser.add_argument('--outf', default='./runs/', help='folder to output images and model checkpoints')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
+
 
 opt = parser.parse_args()
 print(opt)
@@ -140,7 +142,7 @@ class _netG(nn.Module):
 
 class _netG_autoencoder(nn.Module):
     def __init__(self, ngpu):
-        super(netG, self).__init__()
+        super(_netG_autoencoder, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # ENCODER
@@ -177,15 +179,15 @@ class _netG_autoencoder(nn.Module):
             nn.ReLU(True),
             # state size. (ngf*8) x 2 x 2
             nn.ConvTranspose2d(ngf * 8, ngf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
+            nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
+            nn.BatchNorm2d(ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(ngf * 4, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
@@ -258,11 +260,13 @@ if opt.netD != '':
 print(netD)
 
 criterion = nn.BCELoss()
+img_similarity_cirterion = nn.L1Loss()
 
 input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
 input_fake = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
 noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
 fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
+fixed_fake_imgs, _ = dataloader_fake.__iter__().next()
 label = torch.FloatTensor(opt.batchSize)
 real_label = 1
 fake_label = 0
@@ -270,24 +274,30 @@ fake_label = 0
 if opt.cuda:
     netD.cuda()
     netG.cuda()
+    netG_autoencoder.cuda()
     criterion.cuda()
     input, label = input.cuda(), label.cuda()
     input_fake = input_fake.cuda()
     noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
+    fixed_fake_imgs = fixed_fake_imgs.cuda()
+
 
 input = Variable(input)
 input_fake = Variable(input_fake)
 label = Variable(label)
 noise = Variable(noise)
 fixed_noise = Variable(fixed_noise)
+fixed_fake_imgs = Variable(fixed_fake_imgs)
 
 # setup optimizer
 optimizerD = optim.Adam(netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 optimizerG = optim.Adam(netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+optimizerG_autoencoder = optim.Adam(netG_autoencoder.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+
 
 for epoch in range(opt.niter):
-    for i, data in enumerate(dataloader_real, 0):
-        data_fake = dataloader_fake.next()
+    for i, (data, data_fake) in enumerate(izip(dataloader_real, cycle(dataloader_fake)), 0):
+
         ############################
         # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
         ###########################
@@ -299,49 +309,57 @@ for epoch in range(opt.niter):
         batch_size = real_cpu.size(0)
 
         input.data.resize_(real_cpu.size()).copy_(real_cpu)
-        input_fake.data.resize_(input_fake_cpu.size()).copy_(input_fake_cpu)
         label.data.resize_(batch_size).fill_(real_label)
+
+        input_fake.data.resize_(input_fake_cpu.size()).copy_(input_fake_cpu)
 
         output = netD(input)
         errD_real = criterion(output, label)
-        errD_real.backward()
+        # errD_real.backward()
         D_x = output.data.mean()
 
         # train with fake
-        noise.data.resize_(batch_size, nz, 1, 1)
-        noise.data.normal_(0, 1)
-        fake = netG(noise)
+        # noise.data.resize_(batch_size, nz, 1, 1)
+        # noise.data.normal_(0, 1)
+        fake = netG_autoencoder(input_fake)
         label.data.fill_(fake_label)
         output = netD(fake.detach())
         errD_fake = criterion(output, label)
-        errD_fake.backward()
-        D_G_z1 = output.data.mean()
         errD = errD_real + errD_fake
+        errD.backward()
+        D_G_z1 = output.data.mean()
+        
         optimizerD.step()
 
         ############################
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
-        netG.zero_grad()
+        netG_autoencoder.zero_grad()
+        #fake = netG_autoencoder(input_fake)
+
         label.data.fill_(real_label)  # fake labels are real for generator cost
         output = netD(fake)
-        errG = criterion(output, label)
+        errG = criterion(output, label) + img_similarity_cirterion(fake, input_fake)
         errG.backward()
+        #errG_similarity = img_similarity_cirterion(fake, input_fake)
+        #errG_similarity.backward()
         D_G_z2 = output.data.mean()
-        optimizerG.step()
+        optimizerG_autoencoder.step()
+
 
         print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-              % (epoch, opt.niter, i, len(dataloader),
+              % (epoch, opt.niter, i, len(dataloader_real),
                  errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
-        if i % 100 == 0:
+        if i % 10000 == 0:
             vutils.save_image(real_cpu,
                     '%s/real_samples.png' % opt.outf,
                     normalize=True)
-            fake = netG(fixed_noise)
+            fake = netG_autoencoder(fixed_fake_imgs)
             vutils.save_image(fake.data,
                     '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
                     normalize=True)
 
-    # do checkpointing
-    torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
-    torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
+    if epoch % 10 == 0:
+        # do checkpointing
+        torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
+        torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (opt.outf, epoch))
